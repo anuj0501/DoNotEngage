e8a42d154f9f17b4aa237626522516913652a603
abhinavv9 <68147871+abhinavv9@users.noreply.github.com>
2022-01-30 03:04:26 +0530

    newsletter complete

--- Client/src/components/UI/NewsLetter/Newsletter.js
+++ Client/src/components/UI/NewsLetter/Newsletter.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+
+//import axios
+import axios from "axios";
 
 //css import
 import classes from "./Newsletter.module.css";
@@ -15,17 +18,56 @@ const NewsletterForm = (props) => {
     styles = classesDark;
   }
 
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+
+  const validateEmail = (email) => {
+    var result = String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+    setError("");
+    if (result) {
+      return true;
+    } else return false;
+  };
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (validateEmail(email)) {
+      axios
+        .post("/newsletter", {
+          email,
+        })
+        .catch(function (error) {
+          setError(error);
+        });
+    } else {
+      if (email === "") {
+        setError("Please enter your email");
+      }else setError("Please enter a valid email");
+    }
+  }
+
   return (
     <div className={styles.background}>
       <div className={styles.container}>
         <form className={styles.formArea}>
-          <h2>Join Our NewsLetter</h2>
+          <h2>Join Our Newsletter</h2>
           <p>latest information at your fingertips!.</p>
           <div className={styles.mainSection}>
-            <input type="email" placeholder="abc@example.com" />
-            <button>Subscribe</button>
+            <input
+              type="email"
+              name="email"
+              placeholder="abc@example.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <button onClick={handleSubmit}>Subscribe</button>
           </div>
         </form>
+        {error && <p className={styles.errorEmail}>{error}</p>}
         <img src={NewsletterPng} alt="Newsletter" />
       </div>
     </div>
